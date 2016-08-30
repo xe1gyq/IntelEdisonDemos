64a1a151399268e5b48822e241dd56632db4b3b3
Abraham Arce <xe1gyq@gmail.com>
2016-08-30 22:04:58 +0000

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- GroveBasics/main.py
+++ GroveBasics/main.py
@@ -13,7 +13,7 @@ import pyupm_i2clcd as lcd
 button = grove.GroveButton(8)
 display = lcd.Jhd1313m1(0, 0x3E, 0x62)
 knob = grove.GroveRotary(0)
-light = grove.GroveLight(0)
+light = grove.GroveLight(1)
 relay = grove.GroveRelay(2)
 
 def SIGINTHandler(signum, frame):
@@ -28,30 +28,34 @@ signal.signal(signal.SIGINT, SIGINTHandler)
 
 if __name__ == '__main__':
 
-    message = "Hi! I'm Main!"
+    message = "ITESM IoT Lab!"
 
     while True:
 
         luxes = light.value()
         luxes = int(luxes)    
 
-        absdeg = knob.abs_deg()
-        print "Abs values: %4d" % int(abs) , " raw %4d" % int(absdeg), "deg = %5.2f" % absrad , " rad "
+        abs = knob.abs_value()
+        messagelcd = "Rotary Value %d" % int(abs)
 
         display.clear()
         display.setColor(luxes, luxes, luxes)
         display.setCursor(0,0)
         display.write(str(message))
-
-        data = {}
-        data['alive'] = "1"
-        data['luxes'] =  luxes
-        data['absdeg'] =  absdeg
-        data['message'] = message
-        dweepy.dweet_for('TheIoTLearningInitiative', data)
+        display.setCursor(1,0)
+        display.write(str(messagelcd))
 
         if button.value() is 1:
             display.setColor(255, 0, 0)
             relay.on()
             time.sleep(.5)
             relay.off()
+
+        data = {}
+        data['alive'] = "1"
+        data['luxes'] =  luxes
+        data['abs'] =  abs
+        data['message'] = message + ' ' + str(luxes)
+        dweepy.dweet_for('TheIoTLearningInitiative', data)
+
+        time.sleep(.25)
